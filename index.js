c07bf40c96e4a8f7fbaeab187ccae9d1c0180bb9
Prashant Rawat <prashantrawat2mx@gmail.com>
2023-02-07 02:13:08 +0530

    cyclic config

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 9000;
+const PORT = proces.env.PORT || 9000;
+
+// Connecting to database
+mongoose.set('strictQuery', false);
+
+const connectDB = async() => {
+    mongoose.connect(process.env.MONGO_URL, 
+      { useNewUrlParser: true, useUnifiedTopology: true }
+      , () => {
+        console.log("Connected to MongoDB")
+      });
+}
 
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("<h1>Welcome to this site</h1>");
 })
 
-
-app.listen(PORT, () => {
-    console.log("Server up and running on PORT " + PORT);
+connectDB.then(() => {
+    app.listen(PORT, () => {
+        console.log("Server up and running on PORT " + PORT);
+    })
 })
